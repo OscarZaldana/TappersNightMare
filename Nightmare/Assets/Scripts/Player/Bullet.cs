e8404fc12d2960c9b7c4df43e2e0e878f8eb7dcf
Unknown <crazyadragon@gmail.com>
2016-10-28 09:59:55 -0700

    Added PlayerCont

---
+++ Nightmare/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+	public float bulletSpeed;
+	float timer;
+
+	void Update()
+	{
+		transform.Translate(Vector3.forward * Time.deltaTime * bulletSpeed);
+
+		timer += Time.deltaTime;
+		if (timer >= 5)
+			Destroy(gameObject);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		other.gameObject.SendMessage("Hurt");
+	}
+}
